ec0c826081ac7b3dd3ec537d55847d55dd52afc9
Raleigh Littles III <raleighlittles@gmail.com>
2022-07-03 00:21:15 -0700

    started working on rust backend..
    now up to opening device with PID/VID

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,18 @@
-
+extern crate libusb;
 
 fn main() {
-    println!("Hello world!");
+
+    let firmware_filename = std::env::args().nth(1).expect("No firmware filename received");
+
+    let mut libusb_context : libusb::Context = libusb::Context::new().unwrap();
+
+    libusb_context.set_log_level(libusb::LogLevel::Debug);
+
+    const usbVendorId : u16 = 0x05a9;
+    const usbProductId : u16 = 0x0580;
+
+    let libusb_dev_handle : Option<libusb::DeviceHandle> = libusb_context.open_device_with_vid_pid(usbVendorId, usbProductId);
+
+    const usbInterfaceNum : u8 = 0;
+
 }
