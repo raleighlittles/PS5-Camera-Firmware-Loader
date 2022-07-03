794020155546ab40be3ab2dcddb42523d6da095c
Raleigh Littles III <raleighlittles@gmail.com>
2022-07-03 00:48:21 -0700

    got up to libusb claiming device..
    minor cmakelists cleanup

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,4 @@
-extern crate libusb;
+
 
 fn main() {
 
@@ -11,8 +11,24 @@ fn main() {
     const usbVendorId : u16 = 0x05a9;
     const usbProductId : u16 = 0x0580;
 
-    let libusb_dev_handle : Option<libusb::DeviceHandle> = libusb_context.open_device_with_vid_pid(usbVendorId, usbProductId);
+    let mut libusb_dev_handle : libusb::DeviceHandle = libusb_context.open_device_with_vid_pid(usbVendorId, usbProductId).unwrap();
 
+    // Device only has one USB 'endpoint'/interface (see `lsusb` output)
     const usbInterfaceNum : u8 = 0;
+
+    /* Check if the device is in use by the kernel driver; if it is, then you'll
+       need to "detach" it so that libusb can claim it.
+       The device should almost never be claimed by the kernel driver unless you're already
+       actively using it */
+
+    if (libusb_dev_handle.kernel_driver_active(usbInterfaceNum).unwrap()) {
+
+        println!("Kernel is currently using device, detaching it before continuing..");
+        libusb_dev_handle.detach_kernel_driver(usbInterfaceNum);
+    }
+
+    libusb_dev_handle.claim_interface(usbInterfaceNum);
+
+    // TODO: Read firmware file in
 
 }
