75ec9bd0a17d483e53299acf0d8e748b9f6ab7e5
Raleigh Littles III <raleighlittles@gmail.com>
2023-06-29 01:15:25 -0700

    some minor cleanup on the C++ version

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,9 +9,11 @@
 
 constexpr static unsigned int CHUNK_SIZE = 512; // Bulk transfers are limited to 512 bytes per USB standard
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         std::cerr << "ERROR: Incorrect number of arguments" << std::endl;
         return -1;
     }
@@ -19,21 +21,23 @@ int main(int argc, char* argv[]) {
     const uint16_t productId = 0x0580;
     const uint16_t vendorId = 0x05a9;
 
-    libusb_context* lusb_context = nullptr;
+    libusb_context *lusb_context = nullptr;
 
     int rc = libusb_init(&lusb_context);
 
-    libusb_set_option(lusb_context, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_DEBUG);
+    libusb_set_option(lusb_context, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_WARNING);
 
-    if (rc != libusb_error::LIBUSB_SUCCESS) {
+    if (rc != libusb_error::LIBUSB_SUCCESS)
+    {
         std::cerr << "ERROR: Failed to initialize libusb, libusb error# = " << rc << std::endl;
         return -1;
     }
 
-    libusb_device_handle* lusb_dev_hndl = libusb_open_device_with_vid_pid(lusb_context, vendorId, productId);
+    libusb_device_handle *lusb_dev_hndl = libusb_open_device_with_vid_pid(lusb_context, vendorId, productId);
 
-    if (!lusb_dev_hndl) {
-        std::cerr << "ERROR: libusb device handler null"  << std::endl;
+    if (!lusb_dev_hndl)
+    {
+        std::cerr << "ERROR: libusb device handler null" << std::endl;
         return -1;
     }
 
@@ -41,8 +45,10 @@ int main(int argc, char* argv[]) {
     const int interfaceNum = 0;
 
     // Can't claim the device if the operating system is using it
-    if (libusb_kernel_driver_active(lusb_dev_hndl, interfaceNum)) {
-        if (libusb_detach_kernel_driver(lusb_dev_hndl, interfaceNum) != libusb_error::LIBUSB_SUCCESS) {
+    if (libusb_kernel_driver_active(lusb_dev_hndl, interfaceNum))
+    {
+        if (libusb_detach_kernel_driver(lusb_dev_hndl, interfaceNum) != libusb_error::LIBUSB_SUCCESS)
+        {
             std::cerr << "ERROR: Failed to detach kernel driver for device. libusb error#= " << rc << std::endl;
             return -1;
         }
@@ -50,7 +56,8 @@ int main(int argc, char* argv[]) {
 
     rc = libusb_claim_interface(lusb_dev_hndl, interfaceNum);
 
-    if (rc != libusb_error::LIBUSB_SUCCESS) {
+    if (rc != libusb_error::LIBUSB_SUCCESS)
+    {
         std::cerr << "ERROR: Failed to claim device, libusb error#=" << rc << std::endl;
         return -1;
     }
@@ -71,10 +78,18 @@ int main(int argc, char* argv[]) {
         for (uint32_t pos = 0; pos < length; pos += CHUNK_SIZE)
         {
             uint16_t size = (CHUNK_SIZE > (length - pos) ? (uint16_t)(length - pos) : CHUNK_SIZE);
+
             firmware_file.read(reinterpret_cast<char *>(chunk.data()), size);
+
             ctrl_transfer_wrapper(lusb_dev_hndl, usbWriteReqType, 0x0, value, index, chunk.data(), size);
 
-            if ((static_cast<uint32_t>(value) + size) > 0xFFFF) index++;
+            // Careful logging here, it'll slow things down enough to where the USB transfer won't complete in time
+            // std::cout << "Transferred " << size << " bytes [" << pos << "," << (pos+size) << "], value=" << value << ", index=" << index << ", size=" << size << std::endl;
+
+            if ((static_cast<uint32_t>(value) + size) > 0xFFFF)
+            {
+                index++;
+            }
 
             value += size;
         }
@@ -86,12 +101,17 @@ int main(int argc, char* argv[]) {
         ctrl_transfer_wrapper(lusb_dev_hndl, usbWriteReqType, 0x0, 0x2200, 0x8018, chunk.data(), 1);
 
         std::cout << "Finished uploading firmware!" << std::endl;
+
+        libusb_close(lusb_dev_hndl);
     }
     else
     {
         std::cout << "Unable to open firmware" << std::endl;
+        libusb_close(lusb_dev_hndl);
+        libusb_exit(lusb_context);
         return -1;
     }
 
+    libusb_exit(lusb_context);
     return 0;
 }
